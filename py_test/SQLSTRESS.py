5433bc91b15f1d8884fd87168418b025bb61ccea
RfL95 <rofl095@gmail.com>
2017-02-06 10:32:45 +0100

    change SQLSTRESS
    change SQLSTRESS

--- py_test/SQLSTRESS.py
+++ py_test/SQLSTRESS.py
@@ -1,14 +1,13 @@
 import pymysql
 
 conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='', db='hotel')
-
 cur = conn.cursor()
+cur.execute("")  # INSERT INTO Statement here
 
-cur.execute("Select * from hotel")
 
+""" Use for Printing
 for row in cur:
     print row
-
-
+"""
 cur.close()
 conn.close()
