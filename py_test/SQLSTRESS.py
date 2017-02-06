9b1806d452510e94686c0457ef7bfbd27506bb66
RfL95 <rofl095@gmail.com>
2017-02-06 11:03:27 +0100

    minor changes
    minor changes

--- py_test/SQLSTRESS.py
+++ py_test/SQLSTRESS.py
@@ -1,12 +1,11 @@
 import pymysql
 
-conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='', db='hotel')
+conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='', db='hotel', autocommit=True)
 
 cur = conn.cursor()
 
 cur.execute("INSERT INTO hotel (id, name, strasse, ort) "
             "VALUES (NULL, 'Hotel x', 'Strasse x', 'Ort x');")  # INSERT INTO Statement here
-conn.commit()  # Commit changes to Database
 
 cur.execute("Select * from hotel")
 print cur.rowcount  # output rowcount
