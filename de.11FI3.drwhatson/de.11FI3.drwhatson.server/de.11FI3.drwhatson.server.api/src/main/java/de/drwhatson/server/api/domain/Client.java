b75de7d53e0098c6a8f278e4575b99dde9180701
CHRISTIAN\Chrissi <christian.h2702@gmail.com>
2017-03-17 11:31:29 +0100

    Added Methods

--- de.11FI3.drwhatson/de.11FI3.drwhatson.server/de.11FI3.drwhatson.server.api/src/main/java/de/drwhatson/server/api/domain/Client.java
+++ de.11FI3.drwhatson/de.11FI3.drwhatson.server/de.11FI3.drwhatson.server.api/src/main/java/de/drwhatson/server/api/domain/Client.java
@@ -1,12 +1,33 @@
 package de.drwhatson.server.api.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Client {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	
 	private String macAddress;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getMacAddress() {
+		return macAddress;
+	}
+
+	public void setMacAddress(String macAddress) {
+		this.macAddress = macAddress;
+	}
 	
 }
