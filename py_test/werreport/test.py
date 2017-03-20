05fe677b677be51477223b622e1f906aa8aefe77
RfL95 <rofl095@gmail.com>
2017-03-20 08:43:44 +0100

    Cleanup

--- py_test/werreport/test.py
+++
@@ -1,4 +0,0 @@
-from uuid import getnode as get_mac
-mac = get_mac()
-
-print ':'.join(("%012X" % mac)[i:i+2] for i in range(0, 12, 2))
---
+++ py_test/werreport/test_werreader.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+from config.config import ConfigParse
+
+
+
+
+class TestWerreader(TestCase):
+
+    def testpath(self):
+        a = ConfigParse()
