e9e8269e6741494db7bac9428092e0c491f724c7
CHRISTIAN\Chrissi <christian.h2702@gmail.com>
2017-05-18 11:47:17 +0200

    Added ConversionService

--- de.11FI3.drwhatson/de.11FI3.drwhatson.server/de.11FI3.drwhatson.server.business/src/main/java/de/drwhatson/server/business/service/ReportRequestToReportConverterService.java
+++ de.11FI3.drwhatson/de.11FI3.drwhatson.server/de.11FI3.drwhatson.server.business/src/main/java/de/drwhatson/server/business/service/ReportRequestToReportConverterService.java
@@ -1,15 +1,60 @@
 package de.drwhatson.server.business.service;
 
+import java.util.Date;
+import java.util.Optional;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.convert.converter.Converter;
 
 import de.drwhatson.server.api.ReportRequest;
+import de.drwhatson.server.api.domain.Application;
+import de.drwhatson.server.api.domain.Client;
 import de.drwhatson.server.api.domain.Report;
+import de.drwhatson.server.api.domain.User;
 
-public class ReportRequestToReportConverterService implements Converter<ReportRequest, Report>{
+public class ReportRequestToReportConverterService implements Converter<ReportRequest, Report> {
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger(ReportRequestToReportConverterService.class);
+	
+	@Inject
+	private ApplicationService applicationService;
+	@Inject
+	private ClientService clientService;
+	@Inject
+	private UserService userService;
 
-	public Report convert(ReportRequest arg0) {
-		
-		return null;
+	public Report convert(ReportRequest reportRequest) {
+		Application application = getApplicationForName(reportRequest.getAppName());
+		User user = getUserForUserName(reportRequest.getUserName());
+		Client client = getClientForMacAddress(reportRequest.getMacAddress());
+		Report report = Report.create(client, user, application, reportRequest.getBucketId(),
+				reportRequest.getEventType(), reportRequest.getAppVersion(), reportRequest.getAppPath(),
+				new Date(reportRequest.getEventTime()));
+		return report;
+	}
+
+	private Application getApplicationForName(String name) {
+		Optional<Application> application = applicationService.getApplicationByName(name);
+		return application.orElseGet(() -> {
+			return Application.create(name);
+		});
+	}
+
+	private User getUserForUserName(String userName) {
+		Optional<User> user = userService.getUserByUsername(userName);
+		return user.orElseGet(() -> {
+			return User.create(userName);
+		});
+	}
+
+	private Client getClientForMacAddress(String macAddress) {
+		Optional<Client> clientOptional = clientService.getClientByMacAddress(macAddress);
+		return clientOptional.orElseGet(() -> {
+			return Client.create(null, macAddress);
+		});
 	}
 
 }
