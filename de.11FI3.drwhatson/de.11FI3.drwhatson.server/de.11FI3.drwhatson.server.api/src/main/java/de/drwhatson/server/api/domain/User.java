f5bde953c21b9b0ab3af050554a671df0b869962
CHRISTIAN\Chrissi <christian.h2702@gmail.com>
2017-05-18 11:49:09 +0200

    Added hashcode and to string

--- de.11FI3.drwhatson/de.11FI3.drwhatson.server/de.11FI3.drwhatson.server.api/src/main/java/de/drwhatson/server/api/domain/User.java
+++ de.11FI3.drwhatson/de.11FI3.drwhatson.server/de.11FI3.drwhatson.server.api/src/main/java/de/drwhatson/server/api/domain/User.java
@@ -12,7 +12,7 @@ public class User {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	
-	private String userName;
+	private String username;
 	
 	public Long getId() {
 		return id;
@@ -20,11 +20,55 @@ public class User {
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getUserName() {
-		return userName;
+	public String getUsername() {
+		return username;
 	}
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public void setUsername(String userName) {
+		this.username = userName;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + "]";
+	}
+	
+	public static User create(Long id, String userName){
+		User user = new User();
+		user.setId(id);
+		user.setUsername(userName);
+		return user;
+	}
+	
+	public static User create(String userName){
+		return create(null, userName);
 	}
 
 }
