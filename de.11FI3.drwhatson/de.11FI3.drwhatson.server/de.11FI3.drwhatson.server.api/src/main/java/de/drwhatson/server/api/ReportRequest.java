2b0460fda4b99032cf74637b4a7c202ab503a530
CHRISTIAN\Chrissi <christian.h2702@gmail.com>
2017-05-18 11:49:32 +0200

    Changes

--- de.11FI3.drwhatson/de.11FI3.drwhatson.server/de.11FI3.drwhatson.server.api/src/main/java/de/drwhatson/server/api/ReportRequest.java
+++ de.11FI3.drwhatson/de.11FI3.drwhatson.server/de.11FI3.drwhatson.server.api/src/main/java/de/drwhatson/server/api/ReportRequest.java
@@ -4,47 +4,75 @@ public class ReportRequest {
 
 	private String appName;
 	private String appPath;
+	private String appVersion;
 	private String userName;
 	private String macAddress;
-	private int reportType;
+	private int eventType;
 	private String bucketId;
 	private Long eventTime;
-	
+
 	public String getAppName() {
 		return appName;
 	}
+
 	public void setAppName(String appName) {
 		this.appName = appName;
 	}
+
 	public String getAppPath() {
 		return appPath;
 	}
+
 	public void setAppPath(String appPath) {
 		this.appPath = appPath;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getMacAddress() {
 		return macAddress;
 	}
+
 	public void setMacAddress(String macAddress) {
 		this.macAddress = macAddress;
 	}
-	public int getReportType() {
-		return reportType;
+
+	public String getBucketId() {
+		return bucketId;
 	}
-	public void setReportType(int reportType) {
-		this.reportType = reportType;
+
+	public void setBucketId(String bucketId) {
+		this.bucketId = bucketId;
 	}
-	public Long getTimestamp() {
+
+	public int getEventType() {
+		return eventType;
+	}
+
+	public void setEventType(int eventType) {
+		this.eventType = eventType;
+	}
+
+	public String getAppVersion() {
+		return appVersion;
+	}
+
+	public void setAppVersion(String appVersion) {
+		this.appVersion = appVersion;
+	}
+
+	public Long getEventTime() {
 		return eventTime;
 	}
-	public void setTimestamp(Long timestamp) {
-		this.eventTime = timestamp;
+
+	public void setEventTime(Long eventTime) {
+		this.eventTime = eventTime;
 	}
 
 }
