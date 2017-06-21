97e93e90e3a800e5da35e9c18e7cb39bd771c2f6
RfL95 <rofl095@gmail.com>
2017-06-21 11:06:07 +0200

    Update Werreader.py
    Added Json support
    minor changes

--- py_test/werreport/werreader.py
+++ py_test/werreport/werreader.py
@@ -1,5 +1,8 @@
 from ConfigParser import SafeConfigParser
-import random
+import os
+from uuid import getnode as get_mac
+import json
+import requests
 class werreader(object):
     def __init__(self):
         self.version = ""
@@ -9,6 +12,8 @@ class werreader(object):
         self.Bucketid = ""
         self.AppName = ""
         self.AppPath = ""
+        self.uname = ""
+        self.mac = ""
 
     def read(self, path):
         self.path = str(path)
@@ -19,6 +24,7 @@ class werreader(object):
         targetEncoding = "utf-8"
         source = open(self.path,"rb")
         target = open(self.path+"fixed", "w")
+        target.write("11111\n")   #If it looks stupid but it works it aint stupid
         target.write(unicode(source.read(), sourceEncoding).encode(targetEncoding))
         target.close()
         source.close()
@@ -29,21 +35,54 @@ class werreader(object):
         fix.write("[wer]\n")
         fix.close()
 
+        self.uname = os.getenv('username')
+
+        # GET PC MAC ADDR
+        mac = get_mac()
+        self.mac = ':'.join(("%012X" % mac)[i:i + 2] for i in range(0, 12, 2))
 
         #READING
         parser = SafeConfigParser()
         parser.read(self.path+"fixed")
+
+        self.AppName = parser.get("wer", "AppName")
+        self.AppPath = parser.get("wer", "AppPath")
 
         self.version = parser.get("wer","Version")
         self.EventType = parser.get("wer","EventType")
         self.EventTime = parser.get("wer","EventTime")
         self.ReportIdentifier = parser.get("wer","ReportIdentifier")
         self.Bucketid = parser.get("wer","Response.BucketId")
-        self.AppName = parser.get("wer","AppName")
-        self.AppPath = parser.get("wer","AppPath")
 
-        return self.version,self.EventType,self.EventTime, self.ReportIdentifier,self.Bucketid,self.AppName,self.AppPath
+        self.EventTime = float(self.EventTime)
 
+
+        return self.AppName,self.AppPath,self.version,self.uname,self.mac,self.EventType,self.Bucketid,self.EventTime
+
+
+os.system("dir C:\ProgramData\Microsoft\Windows\WER\ReportQueue\ /b *.wer /s /A-D /o:gn>Reports.txt")
+
+out = open("out.txt", "w")
+jout = open("json.txt","w")
 
 a = werreader()
-a.read("C:\ProgramData\Microsoft\Windows\WER\ReportArchive\Critical_10.0.14393.1051__778caec5af73fbca2ceb0e035e75a8e59639bb_00000000_1c9c1ac0\Report.wer")
+#x = a.read("C:\ProgramData\Microsoft\Windows\WER\ReportQueue\AppCrash_AD2F1837.HPPrint_7abab9a238d31ce95943fa32488e7ff1e0ef441f_8eaf7b11_2e13e4ac\Report.wer")
+#print x
+with open('Reports.txt') as fp:
+    for line in fp:
+        print str(line)
+        try:
+            x = a.read(str(line.rstrip("\n")))
+            print x
+            print type(x)
+            jsonfile = json.dumps({ "appName": x[0], "appPath": x[1], "Version": x[2], "userName": x[3],"macAddress" : x[4], "eventType" : x[5], "bucketId": x[6], "eventTime": x[7]})
+            jout.write(str(jsonfile+"\n"))
+            out.write(str(x)+"\n")
+
+
+            #Send JSON
+            url = "http://"+ip+":"+port+"/rest/report/"
+        except:
+            "EOF"
+
+
